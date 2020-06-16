400d412923b4ca468d5bd540478dd9537adafd81
Bogdan Lipuš <bolipus@gmail.com>
2020-06-16 22:17:30 +0200

    Update

--- src/main/java/com/example/demo/entities/Employee.java
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
+
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -16,15 +17,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapKeyColumn;
+import javax.persistence.MapKeyEnumerated;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -33,58 +35,53 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Employee {
 
-  @Id
-  @SequenceGenerator(name = "EMP_SEQ", allocationSize = 25, initialValue = 1)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EMP_SEQ")
-  private Long id;
+    @Id
+    @SequenceGenerator(name = "EMP_SEQ", allocationSize = 25, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EMP_SEQ")
+    private Long id;
 
-  private String name;
+    private String name;
 
-  private Long salary;
+    private Long salary;
 
-  @Enumerated(EnumType.STRING)
-  private EmployeeType type;
+    @Enumerated(EnumType.STRING)
+    private EmployeeType type;
 
-  @Temporal(TemporalType.DATE)
-  private Date startDate;
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
 
-  @ManyToOne
-  @JoinColumn(name = "DEP_ID")
-  private Department department;
+    @ManyToOne
+    @JoinColumn(name = "DEP_ID")
+    private Department department;
 
-  @OneToOne
-  @JoinColumn(name = "PSPACE_ID")
-  ParkingSpace parkingSpace;
+    @OneToOne
+    @JoinColumn(name = "PSPACE_ID")
+    ParkingSpace parkingSpace;
 
-  @Embedded
-  private Address address;
+    @Embedded
+    private Address address;
 
-  @ElementCollection
-  @JoinTable(name = "EMP_VACATION",
-      joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"))
-  private Collection<VacationEntry> vacationBookings;
+    @ElementCollection
+    @JoinTable(name = "EMP_VACATION", joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"))
+    private Collection<VacationEntry> vacationBookings;
 
-  @ElementCollection
-  @JoinTable(name = "EMP_NICKNAMES",
-      joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"))
-  private Set<String> nickNames;
+    @ElementCollection
+    @JoinTable(name = "EMP_NICKNAMES", joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"))
+    private Set<String> nickNames;
 
-  @ManyToMany
-  @JoinTable(name = "EMP_PROJ",
-      joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "id"),
-      inverseJoinColumns = @JoinColumn(name = "PROJ_ID", referencedColumnName = "id"))
-  private Collection<Project> projects;
+    @ManyToMany
+    @JoinTable(name = "EMP_PROJ", joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "PROJ_ID", referencedColumnName = "id"))
+    private Collection<Project> projects;
 
+    @OneToMany
+    @JoinTable(name = "EMP_PHONE", joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "PHONE_ID", referencedColumnName = "ID"))
+    private Collection<Phone> phones;
 
-  @OneToMany
-  @JoinTable(name = "EMP_PHONE",
-      joinColumns = @JoinColumn(name = "EMP_ID", referencedColumnName = "ID"),
-      inverseJoinColumns = @JoinColumn(name = "PHONE_ID", referencedColumnName = "ID"))
-  private Collection<Phone> phones;
+    @ElementCollection
+    @CollectionTable(name = "EMP_PHONENUM")
+    @MapKeyEnumerated(EnumType.STRING)
+    @MapKeyColumn(name = "PHONE_TYPE")
+    @Column(name = "PHONE_NUM")
+    private Map<PhoneType, String> phoneNumbers;
 
-  @ElementCollection
-  @CollectionTable(name = "EMP_PHONENUM")
-  @MapKeyColumn(name = "PHONE_TYPE")
-  @Column(name = "PHONE_NUM")
-  private Map<String, String> phoneNumbers;
 }
